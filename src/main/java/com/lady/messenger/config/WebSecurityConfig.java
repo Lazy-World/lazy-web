a43101e132761245975c3fa60ab5631fd3c69138
Your Death <62382080+zero-src@users.noreply.github.com>
2023-04-11 21:10:23 +0300

    Removed deprecated "WebSecurityConfigurerAdapter" class
    - Fixed captcha not working

--- src/main/java/com/lady/messenger/config/WebSecurityConfig.java
+++ src/main/java/com/lady/messenger/config/WebSecurityConfig.java
@@ -1,46 +1,47 @@
 package com.lady.messenger.config;
 
 import com.lady.messenger.service.UserServiceImpl;
-import com.lady.messenger.service.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserServiceImpl userService;
+public class WebSecurityConfig {
+    private final UserServiceImpl userService;
+    private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Override
-    protected void configure(HttpSecurity httpPageAccess) throws Exception {
-        httpPageAccess
-                .authorizeRequests()
-                    .antMatchers("/", "/registration", "/static/**", "/activate/*", "/img/**").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                .and()
-                    .rememberMe()
-                .and()
-                    .logout()
-                    .permitAll();
+    public WebSecurityConfig(UserServiceImpl userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .antMatchers("/", "/registration", "/static/**", "/activate/*", "/img/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .rememberMe()
+                .and()
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
                 .passwordEncoder(passwordEncoder);
     }
--- src/main/java/com/lady/messenger/dto/CaptchaResponseDto.java
+++ src/main/java/com/lady/messenger/dto/CaptchaResponseDto.java
@@ -2,15 +2,29 @@ package com.lady.messenger.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Value;
 
-import javax.validation.Valid;
 import java.util.Set;
 
-@Value
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CaptchaResponseDto {
-    boolean success;
+    private boolean success;
+
     @JsonAlias("error-codes")
-    Set<String> errorCodes;
+    private Set<String> errorCodes;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Set<String> getErrorCodes() {
+        return errorCodes;
+    }
+
+    public void setErrorCodes(Set<String> errorCodes) {
+        this.errorCodes = errorCodes;
+    }
 }
